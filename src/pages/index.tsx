13f6a11521c918722168f0564775f6f850bacff9
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-07-10 12:16:11 +0300

    Head component for meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // @ts-nochec
 import React, { useState, useEffect } from "react";
 import { Poppins } from "next/font/google";
+import Head from "next/head";
 import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
@@ -45,49 +46,68 @@ export default function Home() {
   }, []);
 
   return (
-    <main className={`${poppins.className}`}>
+    <>
+      <Head>
+        <title>Home - Quotes App</title>
+        <base href="/" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Get motivational quotes to keep you moving and encouraged through quotes app."
+        />
+        <meta
+          name="keywords"
+          content="Quotes App, Motivational quotes, Quotes ,Sayings, Motivation"
+        />
+        <meta name="author" content="Quotes App" />
+      </Head>
       <div className="container">
-        <Navbar />
-        <section className="py-24 sm:py-32 flex items-center justify-center">
-          <div className="w-full sm:w-4/5 text-black">
-            <p className="font-medium text-sm sm:text-lg">
-              {loading ? (
-                <Loading>
-                  <span>Loading quote...</span>
-                </Loading>
-              ) : (
-                <>
-                  <span>
-                    <RiDoubleQuotesL size={30} />
-                  </span>
-                  {quote.quote}
-                  <span>
-                    <RiDoubleQuotesR size={30} className="inline ml-2" />
-                  </span>
-                </>
-              )}
-            </p>
-            <span className="block text-end mt-8 me-10 italic">
-              {loading ? (
-                <Loading>
-                  <span>Loading author...</span>
-                </Loading>
-              ) : (
-                <> ~ {quote.author}</>
-              )}
-            </span>
-            <div className="text-center">
-              <button
-                onClick={getQuote}
-                className="bg-primary rounded-lg py-4 px-8 text-center mt-12 sm:hover:bg-black text-white transition-all duration-1000"
-              >
-                Get new quote
-              </button>
-            </div>
-          </div>
-        </section>
-        <Footer />
+        <div className={`${poppins.className}`}>
+          <Navbar />
+          <main>
+            <section className="py-24 sm:py-32 flex items-center justify-center">
+              <div className="w-full sm:w-4/5 text-black">
+                <p className="font-medium text-sm sm:text-lg">
+                  {loading ? (
+                    <Loading>
+                      <span>Loading quote...</span>
+                    </Loading>
+                  ) : (
+                    <>
+                      <span>
+                        <RiDoubleQuotesL size={30} />
+                      </span>
+                      {quote.quote}
+                      <span>
+                        <RiDoubleQuotesR size={30} className="inline ml-2" />
+                      </span>
+                    </>
+                  )}
+                </p>
+                <span className="block text-end mt-8 me-10 italic">
+                  {loading ? (
+                    <Loading>
+                      <span>Loading author...</span>
+                    </Loading>
+                  ) : (
+                    <> ~ {quote.author}</>
+                  )}
+                </span>
+                <div className="text-center">
+                  <button
+                    onClick={getQuote}
+                    className="bg-primary rounded-lg py-4 px-8 text-center mt-12 sm:hover:bg-black text-white transition-all duration-1000"
+                  >
+                    Get new quote
+                  </button>
+                </div>
+              </div>
+            </section>
+          </main>
+          <Footer />
+        </div>
       </div>
-    </main>
+    </>
   );
 }
--- src/pages/more-quotes.tsx
+++ src/pages/more-quotes.tsx
@@ -1,31 +1,53 @@
 // @ts-nocheck
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Link from "next/link";
+import Head from "next/head";
 import { BiArrowBack } from "react-icons/bi";
 import QuoteText from "@/components/quote-text";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Footer from "@/components/footer";
 import { QuoteProps } from "../../type";
 
 export default function MoreQuotes({
   quotes,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div className="container">
-      <header className="py-6">
-        <nav>
-          <Link
-            href="/"
-            className="font-bold text-black hover:text-primary transition-all duration-1000"
-          >
-            <BiArrowBack size={40} />
-          </Link>
-        </nav>
-      </header>
-      <section className="py-16 sm:py-24 flex flex-col items-center justify-center">
-        {quotes?.quotes.map((quote: QuoteProps, index: number) => (
-          <QuoteText key={index} quote={quote} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>More Quotes - Quotes App</title>
+        <base href="/more-quotes" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Get motivational quotes to keep you moving and encouraged through quotes app."
+        />
+        <meta
+          name="keywords"
+          content="Quotes App, Motivational quotes, Quotes ,Sayings, Motivation"
+        />
+        <meta name="author" content="Quotes App" />
+      </Head>
+      <div className="container">
+        <header className="py-6">
+          <nav>
+            <Link
+              href="/"
+              className="font-bold text-black hover:text-primary transition-all duration-1000"
+            >
+              <BiArrowBack size={40} />
+            </Link>
+          </nav>
+        </header>
+        <main>
+          <section className="my-12 sm:my-16 flex flex-col items-center justify-center">
+            {quotes?.quotes.map((quote: QuoteProps, index: number) => (
+              <QuoteText key={index} quote={quote} />
+            ))}
+          </section>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
